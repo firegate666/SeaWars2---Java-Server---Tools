29fb43a671040968901581c990607a1c95c3cf97
sw <sw>
2005-05-24 21:01:25 +0000

    LGPL Lizenz

--- welterstellung/Inselverteilung.java
+++ welterstellung/Inselverteilung.java
@@ -1,4 +1,22 @@
 /**
+ * This Package allows you to spread a variable Number of Points
+ * over a field of 1000 by 1000 units
+ * Copyright (C) 2005  Andreas Wagener
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * The Author can be contacted by writing an email to "hohlebirne@yahoo.de"
+ *________________________________________________________________________________ 
+ * 
  * VerteilungAusfuehren() aus Konstruktor entfernt
  * Programmaufruf nach instatiierung ausführen
  * besser Lösung statt überladene Konstruktoren wären getter und setter
