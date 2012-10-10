7663108c1c1bafb0dd140883413a87f6b5cf01c3
Marco Behnke <marco@behnke.biz>
2012-10-10 09:47:57 +0200

    remove default methods

--- src/de/seawars/gui/Mainframe.java
+++ src/de/seawars/gui/Mainframe.java
@@ -7,20 +7,11 @@ import de.mb.swing.JDefaultFrame;
 public class Mainframe extends JDefaultFrame{
 
 	PixelFarm iv=new PixelFarm(this);;
-	
-	public void setBounds(int arg0, int arg1, int arg2, int arg3) {
-		super.setBounds(arg0, arg1, arg2, arg3);
-	}
-	public void show(boolean arg0) {
-		this.setVisible(true);
-		super.show();
-	}
+
 	public Mainframe(){
 		super();
-		initialize(); 
-	}
-	public void initialize(){
 		this.getContentPane().setLayout(new BorderLayout() );
 		this.getContentPane().add(iv,BorderLayout.CENTER );
 	}
+
 }
