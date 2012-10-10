8eeee5b3fb99ee4075fea43e0fb5efd83a8c8a85
Marco Behnke <marco@behnke.biz>
2012-10-10 09:47:41 +0200

    add dbparams, remove default args, update kartenabschnitt creation and welt creation

--- src/welterstellung/Welterstellung.java
+++ src/welterstellung/Welterstellung.java
@@ -10,9 +10,11 @@ import java.util.Date;
 import de.mb.database.SQLAnswerTable;
 import de.mb.database.mysql.MysqlConnectionFactory;
 import de.mb.database.mysql.MysqlSQLExecution;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * Diese Klasse kann eine neue Welt erschaffen. 
+ * Diese Klasse kann eine neue Welt erschaffen.
  * @author Andreas
  *
  */
@@ -25,43 +27,44 @@ public class Welterstellung{
 	public double empfindlichkeit;
 	public String bildPfad;
 	public int kartenabschnitt;
-	
+
 	private MysqlConnectionFactory SQLcf;
 	private MysqlSQLExecution SQLexec;
 	private SQLAnswerTable SQLAntwort;
-	
 
-	
+	public String dbhost;
+	public String dbname;
+	public String dbuser;
+	public String dbpass;
+
+
+
 	public Welterstellung(int kartenabschnittID)
 	{
 		dateiAusgabeFlag = 1;
-		dateiAusgabePfad = "C:\\Inselpositionen.txt";
 		testausgabe = 1;
 		archipelAnzahl = 500;
-		bildPfad = "C:\\Eigene Dateien\\C- Programme\\SeaWars\\Beispiel.bmp";
 		empfindlichkeit = 0.5;
 		this.kartenabschnitt = kartenabschnittID;
 
 	}
-	
+
 	/**
-	 * Öffnet einen Kanal in die Datenbank. 
-	 * @return gibt 1 zurück, wenn es nicht geklappt hat, sonst 0.
+	 * ï¿½ffnet einen Kanal in die Datenbank.
+	 * @return gibt 1 zurï¿½ck, wenn es nicht geklappt hat, sonst 0.
 	 */
 	public int OpenConn(){
 		try {
-			String url = "localhost";//"athen062";
-			String dbname = "usr_web4_2";
-			SQLcf = new MysqlConnectionFactory(url, dbname);
-			SQLexec = new MysqlSQLExecution(SQLcf.getConnection("web4", "sw666#GHf"));
+			SQLcf = new MysqlConnectionFactory(dbhost, dbname);
+			SQLexec = new MysqlSQLExecution(SQLcf.getConnection(dbuser, dbpass));
 
 		} catch (SQLException e1) {
-			e1.printStackTrace();
+			//e1.printStackTrace();
 			return 1;
 		}
 		return 0;
 	}
-	
+
 	public void CloseConn(){
 		try {
 			SQLexec.close();
@@ -90,14 +93,14 @@ public class Welterstellung{
 		}
 
 	}
-	
+
 	/**
 	 * Die Methode gibt einen String in eine Textdatei aus.
 	 * @param ausgabe ist der Auszugebende String
 	 * @param pfad ist der Pfad, an den die Datei geschrieben werden soll
-	 * @return 0: Hat funktioniert, 
+	 * @return 0: Hat funktioniert,
 	 * 1: konnte nicht erstellt werden da die Textdatei nicht gefunden wurde
-	 * 2: Konnte aufgrund einer IOException nicht erstellt werden. 
+	 * 2: Konnte aufgrund einer IOException nicht erstellt werden.
 	 */
 	public int dateiAusgabe(String ausgabe, String pfad)
 	{
@@ -106,26 +109,51 @@ public class Welterstellung{
         	FileOutputStream fout = new FileOutputStream(file);
         	fout.write(ausgabe.getBytes());
         	fout.close();
-        } 
+        }
         catch (FileNotFoundException e) {
 	            System.out.println("Sorry, die Datei "+pfad+" konnte nicht erstellt werden");
 	            return 1;
-        } 
+        }
         catch (IOException e) {
 	            System.out.println("Sorry, die Datei konnte nicht erstellt werden");
 	            return 2;
         }
         return 0;
 	}
-	
+
 	public int Erstellung(){
-		if (OpenConn()==1){//Datenbankanbindung öffnen wenn möglich.
-			if (testausgabe > 0) System.out.println("Die Datenbankverbindung konnte nicht geöffnet werden.");
-			return 1; 
+		if (bildPfad == null) {
+			System.err.println("Bildpfad nicht gesetzt");
+			System.exit(1);
+		}
+
+		File f = new File(bildPfad);
+		if (!f.exists()) {
+			System.err.println("Bildpfad '" + bildPfad + "' existiert nicht");
+			System.exit(1);
+		}
+
+
+		if (OpenConn()==1){//Datenbankanbindung ï¿½ffnen wenn mï¿½glich.
+			if (testausgabe > 0) System.out.println("Die Datenbankverbindung konnte nicht geï¿½ffnet werden.");
+			return 1;
 		}
 		long zeitstempel = new Date().getTime();
-		/* Zunächst wird das neueWelt-Objekt erzeugt, das die Archipelpositionen und -größen enthält.
+		/* Zunï¿½chst wird das neueWelt-Objekt erzeugt, das die Archipelpositionen und -grï¿½ï¿½en enthï¿½lt.
 		 */
+		SendQuery("SELECT max(id) FROM welt;");
+		int welt_id = 1;
+		try
+		{
+			welt_id = Integer.parseInt(SQLAntwort.getDataCell(1, 1));
+		}catch(NumberFormatException e) {
+			try {
+				SQLexec.executeInsert("INSERT INTO welt (name) VALUES ('Erste Welt')");
+			} catch (SQLException ex) {
+				Logger.getLogger(Welterstellung.class.getName()).log(Level.SEVERE, null, ex);
+			}
+		}
+
 		SendQuery("SELECT max(id) FROM kartenabschnitt;");
 		try
 		{
@@ -134,20 +162,28 @@ public class Welterstellung{
 		catch (NumberFormatException e)
 		{
 			if (testausgabe>0) System.out.println(e);
-			kartenabschnitt = 1;
+			kartenabschnitt = 0;
 		}
+		if (testausgabe > 0) System.out.println("NÃ¤chster Kartenabschnitt " + kartenabschnitt);
 		try
 		{
-			SQLexec.executeInsert("INSERT LOW_PRIORITY INTO kartenabschnitt (id,  kartennummer, links_id) VALUES ("+
-					kartenabschnitt +", "+ kartenabschnitt+ ", "+ (kartenabschnitt-1)+ ");");
+			if (kartenabschnitt == 0) {
+				SQLexec.executeInsert("INSERT LOW_PRIORITY INTO kartenabschnitt (id,  kartennummer, links_id, welt_id) VALUES ("+
+						kartenabschnitt +", "+ kartenabschnitt+ ", "+ kartenabschnitt+ ", " + welt_id+ ");");
+
+			} else {
+				SQLexec.executeInsert("INSERT LOW_PRIORITY INTO kartenabschnitt (id,  kartennummer, links_id, welt_id) VALUES ("+
+						kartenabschnitt +", "+ kartenabschnitt+ ", "+ (kartenabschnitt-1)+ ", " + welt_id+ ");");
+			}
 			if (kartenabschnitt > 1)
 				SQLexec.executeUpdate("UPDATE LOW_PRIORITY kartenabschnitt SET rechts_id = "+kartenabschnitt+" WHERE id = " +
 									(kartenabschnitt -1) + ";");
 		}
 		catch(SQLException e)
 		{
-
-		}		
+			System.err.println(SQLexec.getLastQuery());
+			e.printStackTrace();
+		}
 		if (testausgabe>0) System.out.println("Welterstellung gestartet. Archipel werden verteilt...");
 		Archipelverteilung neueWelt;
 		neueWelt = new Archipelverteilung(archipelAnzahl, bildPfad, empfindlichkeit);
@@ -169,28 +205,30 @@ public class Welterstellung{
 			}
 			catch(SQLException e)
 			{
-				//Jaja... Ein bis zwei Archipel gehen in die Hose, aber wen störts?
+				//Jaja... Ein bis zwei Archipel gehen in die Hose, aber wen stï¿½rts?
 				//Es werden vorher auch zwei bis drei Archipel zu viel erzeugt (-;
+				System.err.println(SQLexec.getLastQuery());
+				e.printStackTrace();
 			}
 
 		}
-		if (testausgabe>0) 
+		if (testausgabe>0)
 		{
 			System.out.println("Bisherige Laufzeit: " + ((new Date().getTime())-zeitstempel) + " ms. Pro Archipel sind das " +((new Date().getTime())-zeitstempel)/neueWelt.Archipelzahl + " ms.");
 			System.out.println(neueWelt.Archipelzahl + " Archipel wurden auf der Karte "+kartenabschnitt+" verteilt. Inselverteilung wird gestartet...");
 		}
 
-		/* Archipel sind jetzt auf der Karte verteilt und können in die Datenbank eingetragen werden.
+		/* Archipel sind jetzt auf der Karte verteilt und kï¿½nnen in die Datenbank eingetragen werden.
 		 * Ab hier werden die Inseln in den Archipeln verteilt. Es werden alle Archipele nacheinander
 		 * durchgegangen und zu jedem Archipel die Inseln erzeugt. Aufgrund der vielen 2-D-Operatoren
 		 * kann dieser Vorgang mehrere Minuten in Anspruch nehmen.
-		 * In die Inseltabelle müssen folgende Werte eingetragen werden:
+		 * In die Inseltabelle mï¿½ssen folgende Werte eingetragen werden:
 		 * name, groesse, x_pos, y_pos, archipel_id
-		 * Alle Inseln werden mit nur einem Archipel erzeugt, das aber jeweils aus der Datenbank die 
-		 * neuen Eigenschaften bekommt. Der Grund ist, dass die Methode, die unter testen.java 
+		 * Alle Inseln werden mit nur einem Archipel erzeugt, das aber jeweils aus der Datenbank die
+		 * neuen Eigenschaften bekommt. Der Grund ist, dass die Methode, die unter testen.java
 		 * angewendet wurde und alle Inseln im Hauptspeicher hielt, den Rechner doch arg stark aus-
 		 * gelastet hat. Jetzt entfallen extrem viele Konstruktoraufrufe, was das ganze schon schneller
-		 * machen dürfte.
+		 * machen dï¿½rfte.
 		 */
 		int inselzahl = 0;
 		Archipel archipel = new Archipel();
@@ -201,7 +239,7 @@ public class Welterstellung{
 		SQLAnswerTable SQLAntwortArchipel;
 		SQLAntwortArchipel = SQLAntwort;
 
-		// SQLAntwort zählt beginnend mit der 1! (Schweinerei, wer macht denn sowas? ;-) )
+		// SQLAntwort zï¿½hlt beginnend mit der 1! (Schweinerei, wer macht denn sowas? ;-) )
 		for (int i=1; i<=SQLAntwortArchipel.rowCount(); i++)
 		{
 			archipel.archipelID = Integer.parseInt(SQLAntwortArchipel.getDataCell(1,i));
@@ -210,23 +248,23 @@ public class Welterstellung{
 			archipel.groesse	= Integer.parseInt(SQLAntwortArchipel.getDataCell(4,i));
 			while( archipel.inselnImArchipelVerteilen() == 0);
 			if ((testausgabe>0) && (i%10 ==0)) System.out.println((int)((double)i*100/archipelAnzahl) + "% ");
-			
+
 			for (int j=0; j<archipel.inselAnzahl; j++)
 			{
 				SqlSyntaxName = archipel.insel[j].getName().replaceAll("'","\\\\'");
-				// Lager für die Insel erstellen. Kapazität =0. Egal. Ihr wolltet es so.
+				// Lager fï¿½r die Insel erstellen. Kapazitï¿½t =0. Egal. Ihr wolltet es so.
 				try
 				{
 					SQLexec.executeInsert("INSERT LOW_PRIORITY INTO lager (kapazitaet) VALUES (0);");
 				}
 				catch (SQLException e)
 				{
-					System.out.println ("Es konnte kein Lager für diese Insel erstellt werden: "+ SqlSyntaxName);
+					System.out.println ("Es konnte kein Lager fï¿½r diese Insel erstellt werden: "+ SqlSyntaxName);
 				}
 				SendQuery("SELECT  max(id)FROM lager;");
 				archipelQuery = "INSERT LOW_PRIORITY "+
-				"INTO insel (name, groesse, x_pos, y_pos, archipel_id, lager_id) "+
-				"VALUES('"+SqlSyntaxName+"'"+
+				"INTO insel (spieler_id, name, groesse, x_pos, y_pos, archipel_id, lager_id) "+
+				"VALUES(null, '"+SqlSyntaxName+"'"+
 				", "+ archipel.insel[j].groesse +
 				", "+ archipel.insel[j].x_pos +
 				", "+ archipel.insel[j].y_pos +
@@ -241,15 +279,19 @@ public class Welterstellung{
 				catch(SQLException e)
 				{
 					System.out.println("SQL-Insert funktioniert nicht mit folgendem Query: " + archipelQuery);
+					System.err.println(SQLexec.getLastQuery());
+					e.printStackTrace();
+					System.exit(1);
 				}
 			}
 		}
 		if (testausgabe>0)
-		{ 
+		{
 			System.out.println("Fertig. Dieses Mal wurden " + inselzahl+ " Inseln erzeugt.");
 			System.out.println("gesamte Laufzeit:" + ((new Date().getTime())-zeitstempel) + " ms." );
 		}
-	    CloseConn(); //SQL-Datenbankanbindung wieder schließen
+	    CloseConn(); //SQL-Datenbankanbindung wieder schlieï¿½en
+
 		return 0;
 	} //Verteilung
 }
