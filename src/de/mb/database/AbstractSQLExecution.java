bdd24938ec74d6305928cd8bcbdd6b4abf6cd084
Marco Behnke <marco@behnke.biz>
2012-10-10 09:48:18 +0200

    add last query

--- src/de/mb/database/AbstractSQLExecution.java
+++ src/de/mb/database/AbstractSQLExecution.java
@@ -6,33 +6,39 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import de.mb.database.SQLAnswerTable;
 
 /**
  * This Class ist for easy use of SQL Statements
- * 
+ *
  * @author Marco Behnke
  */
 public abstract class AbstractSQLExecution {
 	protected Connection _conn;
 
+	protected String _lastQuery = "";
+
+	public String getLastQuery() {
+		return this._lastQuery;
+	}
+
 	/**
 	 * Public constructor
-	 * 
+	 *
 	 * @param conn	database connection
 	 */
 	public AbstractSQLExecution(Connection conn){
 		this._conn = conn;
 	}
-	
+
 	/**
 	 * Executes SQL Statement and returns result as AnswerTable
-	 * 
+	 *
 	 * @param query			SQL Statement
 	 * @return				Answertable with results
 	 * @throws SQLException
 	 */
 	public SQLAnswerTable executeQuery(String query) throws SQLException {
+		this._lastQuery = query;
 		Statement stmt = _conn.createStatement();
 		SQLAnswerTable t = new SQLAnswerTable(stmt.executeQuery(query));
 		return t;
@@ -40,12 +46,13 @@ public abstract class AbstractSQLExecution {
 
 	/**
 	 * Executes SQL Update and returns affected rows
-	 * 
+	 *
 	 * @param query			SQL Statement
 	 * @return				number of affected rows
 	 * @throws SQLException
 	 */
 	public int executeUpdate(String query) throws SQLException {
+		this._lastQuery = query;
 		Statement stmt = _conn.createStatement();
 		int rows = stmt.executeUpdate(query);
 		return rows;
@@ -53,12 +60,13 @@ public abstract class AbstractSQLExecution {
 
 	/**
 	 * Executes SQL Insert and returns affected rows
-	 * 
+	 *
 	 * @param query			SQL Statement
 	 * @return				number of affected rows
 	 * @throws SQLException
 	 */
 	public int executeInsert(String query) throws SQLException {
+		this._lastQuery = query;
 		// TODO still the same as executeUpdate
 		Statement stmt = _conn.createStatement();
 		int rows = stmt.executeUpdate(query);
@@ -67,18 +75,19 @@ public abstract class AbstractSQLExecution {
 
 	/**
 	 * Executes SQL Delete and returns affected rows
-	 * 
+	 *
 	 * @param query			SQL Statement
 	 * @return				number of affected rows
 	 * @throws SQLException
 	 */
 	public int executeDelete(String query) throws SQLException {
+		this._lastQuery = query;
 		// TODO still the same as executeUpdate
 		Statement stmt = _conn.createStatement();
 		int rows = stmt.executeUpdate(query);
 		return rows;
 	}
-	
+
 	/**
 	 * private
 	 */
@@ -91,7 +100,7 @@ public abstract class AbstractSQLExecution {
 		}
 		return result+" ";
 	}
-	
+
 	/**
 	 * private
 	 */
@@ -119,7 +128,7 @@ public abstract class AbstractSQLExecution {
 		}
 		return result;
 	}
-	
+
 	/**
 	 * private
 	 */
@@ -129,10 +138,10 @@ public abstract class AbstractSQLExecution {
 		}
 		return buildlist(tables, "FROM", ",");
 	}
-	
+
 	/**
 	 * Builds SQL Statement from given fields
-	 * 
+	 *
 	 * @param fields			list of field to retrieve, if list empty, all fields are retrieved
 	 * @param from				list if tables to select from
 	 * @param where				list of where statments, f.e. name='value'
@@ -153,7 +162,7 @@ public abstract class AbstractSQLExecution {
 
 	/**
 	 * Builds SQL Statement from given fields
-	 * 
+	 *
 	 * @param fields			list of field to retrieve, if list empty, all fields are retrieved
 	 * @param from				list if tables to select from
 	 * @return					SQL Answertable
@@ -165,7 +174,7 @@ public abstract class AbstractSQLExecution {
 
 	/**
 	 * Builds SQL Statement from given fields
-	 * 
+	 *
 	 * @param fields			list of field to retrieve, if list empty, all fields are retrieved
 	 * @param from				list if tables to select from
 	 * @param where				list of where statments, f.e. name='value'
@@ -176,14 +185,14 @@ public abstract class AbstractSQLExecution {
 	public SQLAnswerTable executeQuery(ArrayList fields, ArrayList from, ArrayList where, boolean and) throws SQLException{
 		return executeQuery(fields, from, where, and, new ArrayList());
 	}
-	
+
 	/**
 	 * Closes database connection
-	 * 
+	 *
 	 * @throws SQLException
 	 */
 	public void close() throws SQLException {
 		_conn.close();
 	}
-	
+
 }
